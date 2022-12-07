26b6368e0041686ef8a618a193e6896a2884211a
mikesmithlab <mikesmithlab@gmail.com>
2022-12-07 15:48:56 +0000

    descriptive names

--- campus/generate_module_convenors.py
+++ campus/generate_module_convenors.py
@@ -17,6 +17,11 @@ def get_table_data(table, headings):
             data = pd.concat([data, course])
     return data
 
+def get_emails(df, filepath=DLO_DIR + 'Campus/emails.xlsx'):
+    df_emails = pd.read_excel(filepath)
+    df['username'] = df['Convenor'].map(dict(zip(df_emails.staff, df_emails.email)))
+    df['emails']=df['username'].str.replace("","") + '@exmail.nottingham.ac.uk'
+    return df
 
 
 if __name__=='__main__':
@@ -28,6 +33,6 @@ if __name__=='__main__':
     for table in doc.tables:
        df = pd.concat((df,get_table_data(table, headings)))
 
-    get_emails(df)
+    df = get_emails(df)
 
     df.to_excel(DLO_DIR + '/Campus/module_convenors.xlsx', index=False)
